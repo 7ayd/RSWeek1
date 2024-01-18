c81d7a910a6b8480995f21de7ba58c0d9300c9bd
7ayd <zalashini3@gatech.edu>
2024-01-17 16:10:03 -0800

    updates

---
+++ src/TokenSale.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract TokenSale {
+    uint256 public price;
+    uint256 public CONSTANT totalSupply;
+
+
+    
+}
+
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
